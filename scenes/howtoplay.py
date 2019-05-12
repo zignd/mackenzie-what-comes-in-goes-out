194d2e9bd7ad6f251b5e451cf9be3c72c9f55402
Wolfgang Walder <wolfwalder@gmail.com>
2019-05-12 19:33:56 -0300

    Started imports for HowToPlay scene

--- scenes/howtoplay.py
+++ scenes/howtoplay.py
@@ -1,5 +1,7 @@
-from scenes.names import HOW_TO_PLAY_SCENE
-
+import pygame
+import assets.images as images
+from helpers import scale_pair, load_image
+from scenes.names import START_SCENE, HOW_TO_PLAY_SCENE
 
 class HowToPlayScene:
     def __init__(self, win):
@@ -7,3 +9,6 @@ class HowToPlayScene:
 
     def render(self, **args):
         return {'goto': HOW_TO_PLAY_SCENE}
+        
+        background = load_image(images.BACKGROUND)
+        self.win.blit(background, (0, 0))
