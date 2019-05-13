004377b363d3defcd2875d4da47fab8c768304ff
Wolfgang Walder <wolfwalder@gmail.com>
2019-05-13 11:49:48 -0300

    Updated how to play, spacebar.png and arrowkeys.png

--- scenes/howtoplay.py
+++ scenes/howtoplay.py
@@ -1,14 +1,22 @@
 import pygame
 import assets.images as images
 from helpers import scale_pair, load_image
-from scenes.names import START_SCENE, HOW_TO_PLAY_SCENE
+from .names import START_SCENE, HOW_TO_PLAY_SCENE
 
 class HowToPlayScene:
     def __init__(self, win):
         self.win = win
+        self.button_init_pos_x = 328
+        self.button_init_pos_y = 341
+        self.button_height = 107
+        self.button_space_between = 30
 
     def render(self, **args):
         return {'goto': HOW_TO_PLAY_SCENE}
-        
+
         background = load_image(images.BACKGROUND)
         self.win.blit(background, (0, 0))
+
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return {'goto': START_SCENE}
